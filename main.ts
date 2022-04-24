e6dcb38a31c4f798cbe2ba8f57ea5f4625af1912
Jacob B. Sanders <jacob.sanders@users.noreply.github.com>
2022-04-24 03:05:59 -0500

    [Development] - Simplification, Packaging Extension

--- main.ts
+++ main.ts
@@ -1,17 +1,3 @@
-const Test = async () => {
-    const duration = 5000;
-    const $ = async () => await new Promise(async (resolve) => {
-        process.stdout.write("Awaiting ..." + "\n");
-        await setTimeout[Object.getOwnPropertySymbols(setTimeout)[0]](duration);
-
-        resolve(true);
-    });
-
-    await $();
-
-    process.stdout.write("Complete :D" + "\n");
-};
-
-(async () => await Test())();
+(async () => await import("./src"))();
 
 export {};
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const Main = async () => {
+    const duration = 5000;
+    const $ = async () => await new Promise(async (resolve) => {
+        process.stdout.write("Awaiting ..." + "\n");
+        await setTimeout[Object.getOwnPropertySymbols(setTimeout)[0]](duration);
+
+        resolve(true);
+    });
+
+    await $();
+
+    process.stdout.write("  - Complete :D" + "\n");
+};
+
+(async () => await Main())();
+
+export default Main;
+
+export { Main };
